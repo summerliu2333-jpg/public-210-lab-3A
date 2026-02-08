6ef769a120358a4849b3eb428d0a1e31951d0566
Yang Liu <summerliu2333@gmail.com>
2026-02-07 22:42:42 -0800

    Debugging

--- 3A.cpp
+++ 3A.cpp
@@ -22,20 +22,44 @@ Restaurant createRestaurant(){
     getline(cin, temp.address);
 
     cout << "Enter rating (1-5): ";
-    cin >> temp.rating;   
+    cin >> temp.rating;
 
     cout << "Enter number of seats: ";
     cin >> temp.seatCount;
  
     cout << "Has delivery? (1=yes, 0=no): ";
-    cin >> temp.hasDelivery;
+    cin >> temp.hasDelivery;   
 
     return temp;
+}
+
+void printRestaurant(Restaurant r)
+{
+    cout << "---- Restaurant Info ----";
+    cout << "Name: " << r.name << endl;
+    cout << "Address: " << r.address << endl;
+    cout << "Rating: " << r.rating << endl;
+    cout << "Seats: " << r.seatCount << endl;
+
+    cout << "Delivery: ";
+    if (r.hasDelivery)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    cout << "--------------------";
 }
 
 int main() {
 
     Restaurant R1 = createRestaurant();
-    
+    Restaurant R2 = createRestaurant();
+    Restaurant R3 = createRestaurant();
+    Restaurant R4 = createRestaurant();
+
+    printRestaurant(R1);
+    printRestaurant(R2);
+    printRestaurant(R3);
+    printRestaurant(R4);
    return 0; 
 }
