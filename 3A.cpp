877d555b3a296b38e7cf5472f9a3591e254d80e4
Yang Liu <summerliu2333@gmail.com>
2026-02-07 22:22:10 -0800

    Milestone 1

--- 3A.cpp
+++ 3A.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct Restaurant
+{
+    string name;
+    string address;
+    int rating;
+    int seatCount;
+    bool hasDelivery
+    ;
+};
+
+Restaurant createRestaurant(){
+    Restaurant temp;
+
+    cout << "Enter restaurant name: ";
+    getline(cin, temp.name);
+
+    cout << "Enter address: ";
+    getline(cin, temp.address);
+
+    cout << "Enter rating (1-5): ";
+    cin >> temp.rating;   
+
+    cout << "Enter number of seats: ";
+    cin >> temp.seatCount;
+ 
+    cout << "Has delivery? (1=yes, 0=no): ";
+    cin >> temp.hasDelivery;
+
+    return temp;
+}
 
 int main() {
 
-    cout << "hello, world" << endl;
-
+    Restaurant R1 = createRestaurant();
+    
    return 0; 
 }
